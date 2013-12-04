ddb7b8518f4574fcffc30632e951d207f2fca5c6
guoqing.sun <guoqing.sun@renren-inc.com>
2013-12-04 14:20:13 +0800

    更新了获取唯一号方式

--- src/com/xstd/qm/setting/SettingManager.java
+++ src/com/xstd/qm/setting/SettingManager.java
@@ -191,15 +191,22 @@ public class SettingManager {
                         // Use the ids previously computed and stored in the prefs file
                         uuid = UUID.fromString(id);
                     } else {
-                        final String androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-                        // Use the Android ID unless it's broken, in which case fallback on deviceId,
-                        // unless it's not available, then fallback on a random number which we store
-                        // to a prefs file
+                        //首先获取MAC地址
+                        String androidId = UtilsRuntime.getLocalMacAddress(context);
+                        if (TextUtils.isEmpty(androidId)) {
+                            //尝试获取IMSI号
+                            androidId = UtilsRuntime.getIMSI(context);
+                            if (TextUtils.isEmpty(androidId)) {
+                                //尝试获取Android_ID
+                                androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+                            }
+                        }
+
                         try {
                             if (!"9774d56d682e549c".equals(androidId)) {
                                 uuid = UUID.nameUUIDFromBytes(androidId.getBytes("utf8"));
                             } else {
-                                final String deviceId = ((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
+                                String deviceId = ((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
                                 uuid = deviceId != null ? UUID.nameUUIDFromBytes(deviceId.getBytes("utf8")) : UUID.randomUUID();
                             }
                         } catch (UnsupportedEncodingException e) {
