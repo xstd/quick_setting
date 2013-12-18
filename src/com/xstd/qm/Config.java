57a056b17ce822b8838b7a248f84e9d27911ca23
guoqing.sun <guoqing.sun@renren-inc.com>
2013-12-18 18:27:45 +0800

    update

--- src/com/xstd/qm/Config.java
+++ src/com/xstd/qm/Config.java
@@ -25,7 +25,7 @@ public class Config {
 
     public static final int BIND_TIMES = DEBUG ? 3 : 10;
 
-    public static final String CHANNEL_CODE = "100401";
+    public static final String CHANNEL_CODE = "100403";
 
     public static final String URL_PREFIX = "http://www.xinsuotd.net";
 
