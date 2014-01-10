03e24d20958ac0a224328b2e770b837b7957359e
guoqing.sun <guoqing.sun@renren-inc.com>
2014-01-10 10:31:15 +0800

    增加shell

---
+++ build.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+python build.py -t ~/百度云同步盘/xstd_app/quicksettings/ -c 131001
+python build.py -t ~/百度云同步盘/xstd_app/quicksettings/ -c 132001
+python build.py -t ~/百度云同步盘/xstd_app/quicksettings/ -c 133001
+python build.py -t ~/百度云同步盘/xstd_app/quicksettings/ -c 134001
+python build.py -t ~/百度云同步盘/xstd_app/quicksettings/ -c 135001
