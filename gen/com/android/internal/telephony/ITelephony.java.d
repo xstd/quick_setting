7443dec9af7f312ce43854f02fec4d70747d9c68
guoqing.sun <guoqing.sun@renren-inc.com>
2014-02-02 13:52:28 +0800

    temp save

---
+++ gen/com/android/internal/telephony/ITelephony.java.d
@@ -0,0 +1,3 @@
+/Users/michael/github/quick-settings/gen/com/android/internal/telephony/ITelephony.java: \
+  /Users/michael/github/quick-settings/src/com/android/internal/telephony/ITelephony.aidl 
+
